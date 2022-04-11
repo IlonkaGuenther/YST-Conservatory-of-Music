8d2d3f86ae6689818beb6d3486453b2d7269aaff
IlonkaGuenther <lacig@nus.edu.sg>
2022-04-11 18:02:09 +0800

    YST conservatory-webtour maker assignment
    Ilonka's 2nd project

---
+++ data.js
@@ -0,0 +1,132 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.7304542376842775,
+        "pitch": -0.2943202528476654,
+        "fov": 1.4015018869885738
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0663865342264014,
+          "pitch": -0.013601590605027525,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.37030912471905,
+          "pitch": 0,
+          "title": "Conservatory Foyer",
+          "text": "Assuming this is the entry to the foyer"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.44806544510992197,
+        "pitch": -0.023980070309693957,
+        "fov": 1.4015018869885738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0193508191137592,
+          "pitch": -0.022989794703349276,
+          "rotation": 0,
+          "target": "2-conservatory-lounge"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.019350818838948,
+          "pitch": -0.022989795662756052,
+          "title": "Conservatory Lounge",
+          "text": "Assuming this is the way"
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
